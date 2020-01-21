5ade858c4db9687fabc3f73a07b5597253d21c44
Stas <iamlolylol@gmail.com>
2020-01-21 12:46:17 +0200

    reqwest = { version = "0.10.1", features = ["json", "blocking"] }

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,12 +1,11 @@
 extern crate reqwest;
 extern crate serde_json;
+
 use serde_json::Value;
 
 ///
 /// ### Convenience function for getting JSON from return string via `serde`
 
-pub fn get_json(mut reqwest_res: reqwest::Response) -> Result<Value, Box<dyn std::error::Error>> {
-  let data: Value = serde_json::from_str(&reqwest_res.text()?)?;
-
-  Ok(data)
+pub fn get_json(reqwest_res: reqwest::blocking::Response) -> Result<Value, Box<dyn std::error::Error>> {
+    reqwest_res.json().map_err(|e| e.into())
 }
